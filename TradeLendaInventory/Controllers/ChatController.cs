ea2aa1dafee4982976f08a94afd734e4b8c3fe3f
olorunfemiayanfe@gmail.com <olorunfemiayanfe@gmail.com>
2024-08-17 20:04:47 +0100

    Add project files.

---
+++ TradeLendaInventory/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradeLendaInventory.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TradeLendaInventory/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradeLendaInventory.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductController()
+        {
+            
+        }
+        public IActionResult GetProductList()
+        {
+            return View();
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
